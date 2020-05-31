678fbfe23840f4364de6e3a9c27b786551162b7b
oghen <oghen@DESKTOP-SUABB0G>
2020-05-31 11:21:37 +0200

    app

---
+++ webshop/src/main/java/com/ovo/webshop/SpringBootWebshop.java
@@ -0,0 +1,13 @@
+package com.ovo.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebshop {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebshop.class, args);
+	}
+
+}
